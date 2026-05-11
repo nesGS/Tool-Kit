ac8673afaba868560f27ff215c0230de2340439b
nesGS <nesgonsua@gmail.com>
2026-05-11 19:08:05 +0100

    Feat: añadido presencia de estaciones con incidencias en portada

--- toolkit/home.py
+++ toolkit/home.py
@@ -1,9 +1,20 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask_login import current_user
+from .station_models import Station
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return render_template('home/index.html')
+    stations_with_issues = []
+    if current_user.is_authenticated:
+        stations = Station.query.order_by(Station.name.asc()).all()
+        stations_with_issues = [
+            station for station in stations
+            if station.has_active_breakdowns or station.has_active_interventions
+        ]
 
+    return render_template(
+        'home/index.html',
+        stations_with_issues=stations_with_issues
+    )
